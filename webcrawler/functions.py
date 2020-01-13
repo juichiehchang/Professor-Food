a9fd7c7e58c555902799db71469f4ff74d004127
Jui-Chieh Chang <juichieh.chang@gmail.com>
2020-01-13 10:37:48 +0800

    scroll down

--- webcrawler/functions.py
+++ webcrawler/functions.py
@@ -53,7 +53,7 @@ def refresh_cookie(driver, path_to_cookie):
 
 # Scroll down
 def scroll_down(driver):
-    driver.execute_script("window.scrollTo(0, window.scrollY + 600);")
+    driver.execute_script("window.scrollTo(0, window.scrollY + 400);")
 
 # Scroll up
 def scroll_up(driver):
@@ -202,6 +202,7 @@ def show_img(list_title, path):
 # Select restaurant with the given name
 def select_restaurant(driver, res_name):
     check_ad(driver)
+    scroll_down(driver)
     r = driver.find_element_by_xpath('//span[@class="name fn" and text()="' + res_name + '"]/parent::*/parent::*/parent::*/parent::a')
     sleep(1)
     r.click()
