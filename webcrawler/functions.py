beba04c73e772e9a02e7f1bc7a80c5ccef94e908
Jui-Chieh Chang <juichieh.chang@gmail.com>
2020-01-13 10:30:23 +0800

    short sleep

--- webcrawler/functions.py
+++ webcrawler/functions.py
@@ -202,10 +202,10 @@ def show_img(list_title, path):
 # Select restaurant with the given name
 def select_restaurant(driver, res_name):
     check_ad(driver)
-    r = driver.find_element_by_xpath('//span[@class="name fn" and text()="' + res_name + '"]/parent::span')
+    r = driver.find_element_by_xpath('//span[@class="name fn" and text()="' + res_name + '"]/parent::*/parent::*/parent::*/parent::a')
     sleep(1)
     r.click()
-    sleep(5)
+    sleep(3)
 
 # Get dish lists
 def get_dish_lists(driver):
